191a9a394696eecf28a18a7d75c80bb0a4fa0016
shekix <105061972+shekix@users.noreply.github.com>
2025-02-04 23:56:57 +0530

    Update environment.ts

--- ChatPOC/src/environments/environment.ts
+++ ChatPOC/src/environments/environment.ts
@@ -2,13 +2,7 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyCkHE1itaZZi1dMgk-oOoq7XsOHz3Bi5Qw",
-        authDomain: "chatpoc-22fa0.firebaseapp.com",
-        databaseURL: "https://chatpoc-22fa0-default-rtdb.firebaseio.com",
-        projectId: "chatpoc-22fa0",
-        storageBucket: "chatpoc-22fa0.firebasestorage.app",
-        messagingSenderId: "947516619822",
-        appId: "1:947516619822:web:6f906e21f910e5bfffb3bf"
+        
     }
   };
   
